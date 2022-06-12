3152c7ea2f62b02eb676567b2dd8d5bc72b6c697
dthouhgithub <dthouhmaster@gmail.com>
2022-06-12 20:47:17 +0700

    :tada: add UI sign up

--- src/components/User/SignupComponent.vue
+++ src/components/User/SignupComponent.vue
@@ -1,13 +1,85 @@
 <template>
-  <div>
-    <p>This Signup Component</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-col xs="12" sm="6" offset-sm="3">
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form>
+                <v-layout row>
+                  <v-col xs="12">
+                    <v-text-field
+                      name="email"
+                      label="Email"
+                      id="email"
+                      v-model="email"
+                      type="email"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                </v-layout>
+                <v-layout row>
+                  <v-col xs="12">
+                    <v-text-field
+                      name="password"
+                      label="Password"
+                      id="password"
+                      v-model="password"
+                      type="password"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                </v-layout>
+                <v-layout row>
+                  <v-col xs="12">
+                    <v-text-field
+                      name="confirmPassword"
+                      label="ConfirmPassword"
+                      id="confirmPassword"
+                      v-model="confirmPassword"
+                      type="password"
+                      :rules="[comparePasswords]"
+                    ></v-text-field>
+                  </v-col>
+                </v-layout>
+                <v-layout row>
+                  <v-col xs="12">
+                    <v-btn type="submit">Sign Up</v-btn>
+                  </v-col>
+                </v-layout>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "SignupComponent",
 
-  data: () => ({}),
+  data: () => ({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  }),
+  computed: {
+    comparePasswords() {
+      return this.password !== this.confirmPassword
+        ? "Password do not match"
+        : "";
+    },
+  },
+  methods: {
+    onSignup() {
+      console.log({
+        email: this.email,
+        password: this.password,
+        confirmPassword: this.confirmPassword,
+      });
+    },
+  },
 };
 </script>
